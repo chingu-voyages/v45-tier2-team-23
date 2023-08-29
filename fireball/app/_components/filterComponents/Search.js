864940ebf5012fc11e9a1c69ace7b7d03307208c
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-29 14:47:26 -0700

    adding/removing variables as needed
    lifting state to main page for sort function and removing variables that are unused

--- fireball/app/_components/filterComponents/Search.js
+++ fireball/app/_components/filterComponents/Search.js
@@ -6,7 +6,7 @@ import Sliders from './Sliders';
 
 
 export default function Search(props) {
-    const { setFilteredDataSet, filteredDataSet } = props
+    const { setFilteredDataSet, filteredDataSet, setSortingMethod } = props
     const [ massSliderV1, setMassSliderV1 ] = useState(0);
     const [ massSliderV2, setMassSliderV2 ] = useState(23000000);
     const [ yearSliderV1, setYearSliderV1 ] = useState(500);
@@ -18,8 +18,8 @@ export default function Search(props) {
 
     return (
         <div className='flex flex-col w-52 p-4 border border-slate-700' >
-            <SortDropdown  setFilteredDataSet={setFilteredDataSet} filteredDataSet={filteredDataSet} Sort={Sort} setSort={setSort}/>
-            <FilterInputs filteredDataSet={filteredDataSet} setFilteredDataSet={setFilteredDataSet} nameSearch={nameSearch} setNameSearch={setNameSearch} recSearch={recSearch} setRecSearch={setRecSearch}/>
+            <SortDropdown  setFilteredDataSet={setFilteredDataSet} filteredDataSet={filteredDataSet} Sort={Sort} setSort={setSort} setSortingMethod={setSortingMethod}/>
+            <FilterInputs setFilteredDataSet={setFilteredDataSet} nameSearch={nameSearch} setNameSearch={setNameSearch} recSearch={recSearch} setRecSearch={setRecSearch}/>
             <ResetButton setMassSliderV1={setMassSliderV1} setMassSliderV2={setMassSliderV2} setYearSliderV1={setYearSliderV1} setYearSliderV2={setYearSliderV2} setNameSearch={setNameSearch} setRecSearch={setRecSearch} setSort={setSort} filteredDataSet={filteredDataSet} setFilteredDataSet={setFilteredDataSet} />
             <Sliders massSliderV1={massSliderV1} setMassSliderV1={setMassSliderV1} massSliderV2={massSliderV2} setMassSliderV2={setMassSliderV2} yearSliderV1={yearSliderV1} setYearSliderV1={setYearSliderV1} yearSliderV2={yearSliderV2} setYearSliderV2={setYearSliderV2} setFilteredDataSet={setFilteredDataSet} />
         </div>
