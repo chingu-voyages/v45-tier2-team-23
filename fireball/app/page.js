902c9dabda091f2fb04adf568399bad1d4501d1c
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-09-03 10:04:41 -0700

    Add Display.jsx component

--- fireball/app/page.js
+++ fireball/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
         setSortingMethod={setSortingMethod}
       />
       <Table results={results} />
+      <Display results={results} />
     </main>
   );
 }
