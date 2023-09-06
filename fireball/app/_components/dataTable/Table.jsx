d3119014de1d736eba7700a584728e0528e69d0c
Lakpa Dietz <ldietz086@gmail.com>
2023-09-06 13:35:30 -0700

    Edit table

--- fireball/app/_components/dataTable/Table.jsx
+++ fireball/app/_components/dataTable/Table.jsx
@@ -3,7 +3,7 @@ import TableRow from './TableRow';
 export default function Table({ results }) {
   return (
     <div className='text-sm border border-slate-700 h-[400px] overflow-y-auto'>
-      <table className='table-fixed'>
+      <table className='table-fixed w-full'>
         <thead className='sticky top-0 text-slate-700 bg-slate-100 w-full'>
           <tr>
             <th className='py-2 px-4 text-left'>Name</th>
@@ -13,7 +13,7 @@ export default function Table({ results }) {
             <th className='py-2 px-4 text-left'>Location</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className='w-full'>
           {results.map((nasaObj, i) => (
             <TableRow key={i} nasaObj={nasaObj} />
           ))}
