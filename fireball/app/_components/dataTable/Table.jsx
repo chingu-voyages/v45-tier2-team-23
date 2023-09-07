3066400ad9874ad2849ec7517821a674df371737
Lakpa Dietz <ldietz086@gmail.com>
2023-09-07 11:51:05 -0700

    Fixed hydration error

--- fireball/app/_components/dataTable/Table.jsx
+++ fireball/app/_components/dataTable/Table.jsx
@@ -1,6 +1,19 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import DataTable from 'react-data-table-component';
+import { StyleSheetManager } from 'styled-components';
 
-export default function Table2({ results }) {
+export default function Table({ results }) {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setLoader(false);
+  }, []);
+
+  if (loader) {
+    return <div>Loading...</div>;
+  }
+
   const columns = [
     {
       name: 'Name',
@@ -24,22 +37,21 @@ export default function Table2({ results }) {
     },
     {
       name: 'Mass',
-      selector: (row) => row.mass ?? 'N/A',
+      selector: (row) => row.mass || 'N/A',
       sortable: true,
     },
     {
       name: 'Location',
-      selector: (row) => row.locationInfo?.country ?? 'N/A',
+      selector: (row) => row.locationInfo?.country || 'N/A',
       sortable: true,
     },
   ];
+
+  const shouldForwardProp = (prop) => !prop.startsWith('$');
+
   return (
-    <DataTable
-      // title='Meteorite Strikes'
-      columns={columns}
-      data={results}
-      fixedHeader
-      pagination
-    ></DataTable>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <DataTable columns={columns} data={results} fixedHeader pagination />
+    </StyleSheetManager>
   );
 }
