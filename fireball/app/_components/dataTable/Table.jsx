0315a04ac6658259eb6fb3df7db1583b2afb724b
Lakpa Dietz <ldietz086@gmail.com>
2023-09-06 20:23:17 -0700

    Added new table with sorting and pagination

--- fireball/app/_components/dataTable/Table.jsx
+++ fireball/app/_components/dataTable/Table.jsx
@@ -1,24 +1,45 @@
-import TableRow from './TableRow';
+import DataTable from 'react-data-table-component';
 
-export default function Table({ results }) {
+export default function Table2({ results }) {
+  const columns = [
+    {
+      name: 'Name',
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: 'Year',
+      selector: (row) => {
+        if (row.year) {
+          return row.year.substring(0, 4);
+        }
+        return 'N/A';
+      },
+      sortable: true,
+    },
+    {
+      name: 'Composition',
+      selector: (row) => row.recclass,
+      sortable: true,
+    },
+    {
+      name: 'Mass',
+      selector: (row) => row.mass ?? 'N/A',
+      sortable: true,
+    },
+    {
+      name: 'Location',
+      selector: (row) => row.locationInfo?.country ?? 'N/A',
+      sortable: true,
+    },
+  ];
   return (
-    <div className='text-sm border border-slate-700 h-[400px] overflow-y-auto'>
-      <table className='table-fixed w-full'>
-        <thead className='sticky top-0 text-slate-700 bg-slate-100 w-full'>
-          <tr>
-            <th className='py-2 px-4 text-left'>Name</th>
-            <th className='py-2 px-4 text-left'>Year</th>
-            <th className='py-2 px-4 text-left'>Composition</th>
-            <th className='py-2 px-4 text-left'>Mass</th>
-            <th className='py-2 px-4 text-left'>Location</th>
-          </tr>
-        </thead>
-        <tbody className='w-full'>
-          {results.map((nasaObj, i) => (
-            <TableRow key={i} nasaObj={nasaObj} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <DataTable
+      // title='Meteorite Strikes'
+      columns={columns}
+      data={results}
+      fixedHeader
+      pagination
+    ></DataTable>
   );
 }
