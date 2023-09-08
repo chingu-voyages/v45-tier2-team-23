a54e682b7a0ed8e3997b111bd0109654a2786d0c
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-09-08 09:26:02 -0700

    Clean code

--- fireball/app/_components/summaryMetrics/BarChart.jsx
+++ fireball/app/_components/summaryMetrics/BarChart.jsx
@@ -1,16 +1,9 @@
 import { Bar } from "react-chartjs-2";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Chart from "chart.js/auto";
 
 export default function BarChart({ results }) {
   const [chartType, setChartType] = useState("year");
-
-  const customStyles = {
-    canvas: {
-      width: "100%", // Use 100% width to fill the parent container
-      height: "auto", // Allow the height to adjust based on content
-    },
-  };
 
   // Bar chart - year
   const yearCounts = {};
