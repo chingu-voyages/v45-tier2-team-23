0eec8579a9e1e455530a89255fe634f242a414a2
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-09-16 14:03:01 -0700

    Change font

--- fireball/app/layout.jsx
+++ fireball/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
   title: "FireBall",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
